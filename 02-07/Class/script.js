78aa6eb793a46f4746cd059801469b4a26c30f5f
Vaibhav Pawar <pawarvaibhav0179@gmail.com>
2025-07-02 19:40:27 +0530

    02-07

---
+++ 02-07/Class/script.js
@@ -0,0 +1,77 @@
+// 1. Write a JS function to validate given number is prime number or not
+
+// JS Program :
+// let count = 0;
+// let n = +prompt("Enter the number : ");
+
+// for(let i=1;i<=n;i++)
+// {
+//     if(n % i == 0)
+//     count++;
+// }
+// if(count == 2)
+//     {
+//         console.log("Prime Number.");
+//     }
+//     else
+//     {
+//         console.log("Not Prime Number.");
+//     }
+
+
+// 2. Write a JS function to validate given number is palindrome or not.
+
+// JS Program :
+
+// let n = +prompt("Enter the number : ");
+// let temp = n;
+// let sum = 0;
+		
+// 		while (temp!=0)
+// 		{
+// 			let ld = temp%10;
+
+// 			sum = sum * 10+ ld;
+
+// 			temp = parseInt(temp /10);
+// 		}
+// 		if (sum==n){
+// 			console.log("Palindrome Number");
+// 		}
+// 		else
+// 		{
+// 			console.log("Not a Palindrome Number");
+// 		}
+
+
+// 3. Write a JS function to accept three numbers and check sum of any two numbers equals to third value or not and print the result.
+
+// JS Program :
+// let num1 = +prompt("Enter the first number : ");
+// let num2 = +prompt("Enter the second number : ");
+// let num3 = +prompt("Enter the third number : ");
+
+// if(num1 + num2 == num3)
+// {
+//     console.log(`adding ${num1} and ${num2} result ${num3}`);
+// }
+// else if(num1 + num3 == num2)
+// {
+//     console.log(`adding ${num1} and ${num3} result ${num2}`);
+// }
+// else if(num2 + num3 == num1)
+// {
+//     console.log(`adding ${num2} and ${num3} result ${num1}`);
+// }
+// else
+// {
+//     console.log("Invalid addition");
+// }
+
+
+// 4. Write a JS function to accept a string print no of vowels, consonants and spaces in it.
+
+// JS Program :
+
+// let arr = [5,4,3];
+// arr = [5,4,6,1,3];
